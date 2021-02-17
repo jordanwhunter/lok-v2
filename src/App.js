3d2bc93b4f11be77d0b935b78627ffcb310c960a
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-17 15:36:04 -0500

    Imported AuthProvider into App.js and wrapped other components in it

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from './contexts/AuthContext';
 import Signup from './components/authentication/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Container 
-      className='d-flex align-items-center justify-content-center'
-      style={{ minHeight: '100vh' }}
-    >
-      <div 
-        className='w-100' 
-        style={{ maxWidth: '400px' }}
+    <AuthProvider>
+      <Container 
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: '100vh' }}
       >
-        <Signup />
-      </div>
-    </Container>
+        <div 
+          className='w-100' 
+          style={{ maxWidth: '400px' }}
+        >
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
