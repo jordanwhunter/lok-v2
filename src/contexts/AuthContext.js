32c1df70ca717fb0de508e7a5a402254b75858f6
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-17 15:32:28 -0500

    Created unsubscribe function that's handled only upon mounting/unmounting of component

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
-
+import React, { useState, useEffect, useContext } from 'react';
+import { auth } from '../firebase';
+ 
 const AuthContext = React.createContext();
 
 // Returns context of the AuthContext
@@ -9,8 +10,30 @@ export function useAuth() {
 
 // Children are taken into the provider and renders them out
 export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+
+  function signup(email, password) {
+    // Change this if ever needed to sign up to a different server (instead of using Firebase)
+    return auth.createUserWithEmailAndPassword(email, password)
+  };
+
+  // We don't want this in our render, we only want to run this when we mount the component
+  useEffect(() => {
+    // Firebase's onAuthStateChanged() has its own way of notifying you when the user's state is first set
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+    })
+    // onAuthStateChanged() creates a method that, when called, unsubscribes from the listener whenever component is unmounted
+    return unsubscribe
+  }, []);
+
+  const value = {
+    currentUser,
+    signup
+  };
+
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   )
