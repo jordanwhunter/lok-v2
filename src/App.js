0a9ae7d7c6f064306cf454d5d84c8ea6d72f5830
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-17 14:50:46 -0500

    Imported container for sign up component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Signup from './components/authentication/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Signup />
+    <Container>
+      <Signup />
+    </Container>
   );
 }
 
