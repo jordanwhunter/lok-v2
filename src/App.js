3128bf4e6aab43ad6ccadfe7ffe999e796b85497
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-17 14:54:36 -0500

    Styles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Container>
-      <Signup />
+    <Container 
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <div 
+        className='w-100' 
+        style={{ maxWidth: '400px' }}
+      >
+        <Signup />
+      </div>
     </Container>
   );
 }
