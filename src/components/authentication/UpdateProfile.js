7b79f6dfbe91e879322d1ec037c8ff8f8bdfa4e5
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-18 16:00:08 -0500

    Building structure for updating profile

--- src/components/authentication/UpdateProfile.js
+++ src/components/authentication/UpdateProfile.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
+
 
 export default function UpdateProfile() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const history = useHistory();
+
+  const { currentUser } = useAuth();
+
+  // Setting loading state so user can't hit button multiple times during creation of account
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function handleSubmit(event) {
+    // event.preventDefault()
+
+    // // validation check
+    // if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+    //   return setError('Password entries must match')
+    // }
+
+    // try {
+    //   setError('')
+    //   setLoading(true)
+    //   await signup(
+    //     emailRef.current.value, 
+    //     passwordRef.current.value, 
+    //     passwordConfirmRef.current.value
+    //   )
+    //   history.push('/')
+    // } catch {
+    //   setError('Unable to create account')
+    // }
+    // setLoading(false)
+  };
+
   return (
-    <div>
-      Hello world. Update your profile here.
-    </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Update Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control 
+                type='email' 
+                ref={emailRef} 
+                required
+                defaultValue={currentUser.email}
+              />
+            </Form.Group>
+            <Form.Group id='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type='password' 
+                ref={passwordRef} 
+                required
+                placeholder='Leave blank to keep the same'
+              />
+            </Form.Group>
+            <Form.Group id='password-confirm'>
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control 
+                type='password' 
+                ref={passwordConfirmRef} 
+                required
+                placeholder='Leave blank to keep the same'
+              />
+            </Form.Group>
+            <Button 
+              disabled={loading} 
+              className='w-100' 
+              type='submit'
+            >
+              Update
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Link to='/'>Cancel</Link>
+      </div>
+    </>
   )
-}
+};
+
