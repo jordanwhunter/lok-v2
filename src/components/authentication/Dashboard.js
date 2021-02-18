6e70196a3556133d11b088ac1cadfe790a676e7a
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-18 14:40:12 -0500

    Creating basic dashboard to display logged in username and give option to update profile or logout

--- src/components/authentication/Dashboard.js
+++ src/components/authentication/Dashboard.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Card, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
 
 export default function Dashboard() {
+  const history = useHistory();
+  
+  const [error, setError] = useState('');
+
+  const { currentUser, logout } = useAuth();
+
+  async function handleLogout() {
+    setError('')
+
+    try {
+      await logout()
+      history.push('/login')
+    } catch {
+      setError('Cannot log out')
+    }
+  };
+
   return (
-    <div>
-      Hello World. I'm the dashboard.
-    </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+            Update
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+          <Button 
+            variant='link' 
+            onClick={handleLogout}
+          >
+            Log Out
+          </Button>
+      </div>
+    </>
   )
 }
