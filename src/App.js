e5fb8ae2c865c35dd5f651dac510a576998f7c65
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-18 15:06:19 -0500

    Routed for forgotten password component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './components/authentication/Dashboard';
 import Signup from './components/authentication/Signup';
 import Login from './components/authentication/Login';
 import PrivateRoute from './components/authentication/PrivateRoute';
+import ForgotPassword from './components/authentication/ForgotPassword';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,21 +15,22 @@ function App() {
     className='d-flex align-items-center justify-content-center'
     style={{ minHeight: '100vh' }}
     >
-        <div 
-          className='w-100' 
-          style={{ maxWidth: '400px' }}
-          >
-          <Router>
-            <AuthProvider>
-              <Switch>
-                <PrivateRoute exact path='/' component={Dashboard} />
-                <Route path='/signup' component={Signup} />
-                <Route path='/login' component={Login} />
-              </Switch>
-            </AuthProvider>
-          </Router>
-        </div>
-      </Container>
+      <div 
+        className='w-100' 
+        style={{ maxWidth: '400px' }}
+        >
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path='/' component={Dashboard} />
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
+              <Route path='/forgot-password' component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
