30c6191ee9b3e7ea8e115a1453134e44a08ca970
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-18 14:12:38 -0500

    Building rough outline for login page

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
+
 
 export default function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const { login } = useAuth();
+
+  // Setting loading state so user can't hit button multiple times during creation of account
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    try {
+      setError('')
+      setLoading(true)
+      await login(
+        emailRef.current.value, 
+        passwordRef.current.value
+      )
+    } catch {
+      setError('Cannot log in')
+    }
+    setLoading(false)
+  };
+
   return (
-    <div>
-      Hello World. I'm the login page.
-    </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Log In</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id='email'>
+              <Form.Label>Email</Form.Label>
+              <Form.Control 
+                type='email' 
+                ref={emailRef} 
+                required 
+              />
+            </Form.Group>
+            <Form.Group id='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type='password' 
+                ref={passwordRef} 
+                required 
+              />
+            </Form.Group>
+            <Button 
+              disabled={loading} 
+              className='w-100' 
+              type='submit'
+            >
+              Log In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        Don't have an account? <Link to='/signup'>Sign Up</Link>
+      </div>
+    </>
   )
-}
+};
