ca83ed55992ea4d0bc5c01d0b3b2d49ddaeb2ee8
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-19 16:58:01 -0500

    Set up folder modal

--- src/components/lok/AddFolderButton.js
+++ src/components/lok/AddFolderButton.js
@@ -1,20 +1,72 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
 
 import CreateNewFolderIcon from '@material-ui/icons/CreateNewFolder';
 
 export default function AddFolderButton() {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  
   function openModal() {
+    setOpen(true)
+  };
 
+  function closeModal() {
+    setOpen(false)
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // Create folder within the database
+    setName('')
+    closeModal()
   };
   
   return (
-    <Button 
-      onClick={openModal}
-      variant='outline-success'
-      size='sm'
-    >
-      <CreateNewFolderIcon />
-    </Button>
+    <>
+      <Button 
+        onClick={openModal}
+        variant='outline-success'
+        size='sm'
+      >
+        <CreateNewFolderIcon />
+      </Button>
+
+      <Modal 
+        show={open} 
+        onHide={closeModal}
+        animation={false}
+      >
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Folder Name</Form.Label>
+              <Form.Control
+                type='text'
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button
+              variant='secondary'
+              onClick={closeModal} 
+            >
+              Close
+            </Button>
+            <Button
+              variant='success'
+              type='submit'
+            >
+              Add Folder
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
   )
 };
