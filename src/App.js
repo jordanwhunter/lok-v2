a32ffccf9f3325a48be818c52695b535ef1145b7
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-19 14:08:32 -0500

    Removed styled container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
 import Dashboard from './components/authentication/Dashboard';
 import Signup from './components/authentication/Signup';
@@ -12,28 +11,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Container 
-    className='d-flex align-items-center justify-content-center'
-    style={{ minHeight: '100vh' }}
-    >
-      <div 
-        className='w-100' 
-        style={{ maxWidth: '400px' }}
-        >
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path='/' component={Dashboard} />
-              <PrivateRoute path='/update-profile' component={UpdateProfile} />
-              <Route path='/signup' component={Signup} />
-              <Route path='/login' component={Login} />
-              <Route path='/forgot-password' component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
-  );
-}
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <PrivateRoute exact path='/' component={Dashboard} />
+          <PrivateRoute path='/update-profile' component={UpdateProfile} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/login' component={Login} />
+          <Route path='/forgot-password' component={ForgotPassword} />
+        </Switch>
+      </AuthProvider>
+    </Router>
+  )
+};
 
 export default App;
