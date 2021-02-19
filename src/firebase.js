1f4582a7515b10032f5e84315c287b253076a941
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-19 17:09:15 -0500

    Adding firestore parameters and exports to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,5 +12,14 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
+const firestore = app.firestore()
+
 export const auth = app.auth()
+
+// limiting what user has access to within the firestore
+export const db = {
+  folders: firestore.collection('folders'),
+  files: firestore.collection('files')
+}
+
 export default app
