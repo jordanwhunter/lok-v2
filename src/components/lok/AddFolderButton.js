4ed9d5a98ad6968da21a292cae0a2f12def43fa9
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-19 16:12:44 -0500

    Creating button component for adding a folder in which user can then add a file inside of

---
+++ src/components/lok/AddFolderButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export default function AddFolderButton() {
+  function openModal() {
+
+  };
+  
+  return (
+    <Button onClick={openModal}>
+      
+    </Button>
+  )
+};
