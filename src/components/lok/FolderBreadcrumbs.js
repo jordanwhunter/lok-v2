ddca7a1cccc7314e7ee2ea98c7304ee2f3909738
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-22 14:18:36 -0500

    Refactor

--- src/components/lok/FolderBreadcrumbs.js
+++ src/components/lok/FolderBreadcrumbs.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import { Breadcrumb } from 'react-bootstrap';
 
 export default function FolderBreadcrumbs({ currentFolder }) {
   return (
-    <div>
-      Hello world. I am the component related to Folder Breadcrumbs.
-    </div>
+    <Breadcrumb />
   )
 };
