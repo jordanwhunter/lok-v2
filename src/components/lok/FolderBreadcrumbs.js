aa5f906e2bdf1bebb9481bbeabaeba197a2ace53
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-22 14:25:37 -0500

    Styling breadcrumb

--- src/components/lok/FolderBreadcrumbs.js
+++ src/components/lok/FolderBreadcrumbs.js
@@ -3,6 +3,19 @@ import { Breadcrumb } from 'react-bootstrap';
 
 export default function FolderBreadcrumbs({ currentFolder }) {
   return (
-    <Breadcrumb />
+    <Breadcrumb 
+      className='flex-grow-1'
+      listProps={{ className: 'bg-white pl-0 m-0'}}
+    >
+      {currentFolder && (
+        <Breadcrumb.Item 
+          classname='text-truncated-inline-block' 
+          style={{ maxWidth: '150px' }}
+          active
+        >
+          {currentFolder.name}
+        </Breadcrumb.Item>
+      )}
+    </Breadcrumb>
   )
 };
