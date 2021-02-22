73a9010f4e8468a5116e7546589eb7d2932e408d
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-22 18:02:28 -0500

    Created AddFileButton component

---
+++ src/components/lok/AddFileButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AddFileButton() {
+  return (
+    <div>
+      Hello world. I'm a component for adding files to our database.
+    </div>
+  )
+};
