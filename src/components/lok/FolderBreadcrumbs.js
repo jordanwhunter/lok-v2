6966b7dddf79613e04e5fe2da5a7135e63544884
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-22 14:17:08 -0500

    Creating component for FolderBreadcrumbs

---
+++ src/components/lok/FolderBreadcrumbs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FolderBreadcrumbs({ currentFolder }) {
+  return (
+    <div>
+      Hello world. I am the component related to Folder Breadcrumbs.
+    </div>
+  )
+};
