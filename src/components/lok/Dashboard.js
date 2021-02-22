e49a1c5ab09912c23638ea1e278d851446e66ae2
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-22 13:01:25 -0500

    Designate currentFolder on Dashboard

--- src/components/lok/Dashboard.js
+++ src/components/lok/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useFolder } from '../../hooks/useFolder';
 import Navbar from './Navbar';
 import AddFolderButton from './AddFolderButton';
 
 export default function Dashboard() {
+  const { folder } = useFolder();
+  
   return (
     <>
       <Navbar />
       <Container fluid>
-        <AddFolderButton />
+        <AddFolderButton currentFolder={folder} />
       </Container>
     </>
   )
