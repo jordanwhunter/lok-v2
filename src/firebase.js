2115865cf42db5b02e6c231db71c7a386e408b58
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-23 12:02:20 -0500

    import firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,7 +15,7 @@ const app = firebase.initializeApp({
 
 const firestore = app.firestore()
 
-export const auth = app.auth()
+export const auth = app.auth();
 
 // limiting what user has access to within the firestore
 export const db = {
@@ -24,6 +25,8 @@ export const db = {
     return { id: doc.id, ...doc.data() }
   },
   getCurrentTimeStamp: firebase.firestore.FieldValue.serverTimestamp,
-}
+};
 
-export default app
+export const storage = app.storage();
+
+export default app;
