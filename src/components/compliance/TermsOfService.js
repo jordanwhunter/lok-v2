c04a47c422661229e83473780e0cfc1c41674a03
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-02-24 15:06:22 -0500

    Defined Terms of Service

--- src/components/compliance/TermsOfService.js
+++ src/components/compliance/TermsOfService.js
@@ -1,9 +1,165 @@
 import React from 'react';
+import { Card, ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import LockTwoToneIcon from '@material-ui/icons/LockTwoTone';
 
 export default function TermsOfService() {
   return (
-    <div>
-      Hello world. I'm the Terms of Service component.
-    </div>
+    <Card>
+      <Card.Body>
+        <Card.Title style={{ textAlign: 'center' }}>
+          <LockTwoToneIcon /> 
+          <hr />
+        </Card.Title>
+        <Card.Subtitle style={{ textAlign: 'center' }}>
+          <h3>Terms of Service</h3>
+        </Card.Subtitle>
+        <Card.Text>
+          <strong>Lok Terms of Service</strong> 
+          <br /><br />
+
+          Effective: February 24, 2021
+          <br /><br />
+
+          <u>If you use this storage system, that means you agree to these <strong>Terms of Service </strong>detailed below.</u>
+          <br /><br />
+
+          Thanks for using Lok! Our mission is to create a more direct way of working by providing a platform to keep your content safe and accessible. These terms of service (“<strong>Terms</strong>”) cover your use and access to our service ("<strong>Service</strong>"). This agreement applies worldwide, regardless of geological location. Our <Link to='/privacy-policy' target='_blank'>Privacy Policy</Link> explains how we collect and use your information while our <Link to='/acceptable-use' target='_blank'>Acceptable Use Policy</Link> outlines your responsibilities when using our Service. <u>By using our Service, you’re agreeing to be bound by these Terms, our <Link to='/privacy-policy' target='_blank'>Privacy Policy</Link>, and <Link to='/acceptable-use' target='_blank'>Acceptable Use Policy</Link>.</u>
+          <br /><br />
+
+          <strong>Your Things & Your Permissions</strong>
+          <br /><br />
+
+          When you use our Service, you provide us with stuff like your files, content, gists, and so on (“<strong>Your Things</strong>”). Your Things are yours. These Terms don’t give us any rights to Your Things except for the limited rights that enable us to offer this Service.
+          <br /><br />
+          We need your permission to do stuff like hosting Your Things and sharing them when you ask us to. Our Service also provides you document / image / video previews. To provide these and other features, Lok accesses, stores, and scans Your Things. You give us permission to do that, and this permission extends to our affiliates and trusted third parties we work with.
+          <br /><br />
+
+          <strong>Your Responsibilities</strong>
+          <br /><br />
+
+          Your use of our Service must comply with our <Link to='/acceptable-use' target='_blank'>Acceptable Use Policy</Link>. Content in this Service may be protected by others’ intellectual property rights. Please don’t copy, upload, download, or share content unless you have the right to do so.
+          <br /><br />
+          Lok may review your conduct and content for compliance with these Terms and our <Link to='/acceptable-use' target='_blank'>Acceptable Use Policy</Link>. <u>We aren’t responsible for the content people post and share via this Service.</u>
+          <br /><br />
+          Help us keep Your Things protected. Safeguard your password to the Service, and keep your account information current. Don’t share your account credentials or give others access to your account.
+          <br /><br />
+          You may use our Service only as permitted by applicable law, including export control laws and regulations. Finally, to use our Service, you must be at least 13 (or older, depending on where you live).
+          <br /><br />
+
+          <strong>Termination</strong>
+          <br /><br />
+
+          You’re free to stop using our Service at any time. We reserve the right to suspend or terminate your access to the Services without notice to you if:
+        </Card.Text>
+
+        <ListGroup variant='flush'>
+          <ListGroup.Item>(a) you’re in breach of these Terms,</ListGroup.Item>
+          <ListGroup.Item>(b) your use of the Service would cause a real risk of harm or loss to us or other users, or</ListGroup.Item>
+          <ListGroup.Item>(c) you haven't accessed our Service for 12 consecutive months.</ListGroup.Item>
+        </ListGroup>
+        <br /><br />
+
+        <Card.Text>
+          In special cases, we may send some notification to let you know that your account is scheduled for termination, but that is solely at the discretion of Lok. We absolutely will not provide notice before termination where:
+        </Card.Text>
+
+        <ListGroup variant='flush'>
+          <ListGroup.Item>(a) you’re in material breach of these Terms,</ListGroup.Item>
+          <ListGroup.Item>(b) doing so would cause us legal liability or compromise our ability to provide the Service to our other users, or</ListGroup.Item>
+          <ListGroup.Item>(c) we're prohibited from doing so by law.</ListGroup.Item>
+        </ListGroup>
+        <br />
+
+        <Card.Text>
+          <strong>Discontinuation of Service</strong>
+          <br /><br />
+
+          Due to Lok being a free service, we may decide at any time to discontinue the Service (entirely or specifically to a user) in response to unforeseen circumstances beyond our control, or to comply with a legal requirement. Lok is not responsible for data lost in any capacity due to this.
+          <br /><br />
+
+          <strong>Service “AS IS”</strong>
+          <br /><br />
+
+          We strive to provide a great Service, but there are certain things that we can't guarantee. TO THE FULLEST EXTENT PERMITTED BY LAW, LOK MAKES NO WARRANTIES, EITHER EXPRESS OR IMPLIED, ABOUT THE SERVICE. THE SERVICE IS PROVIDED "AS IS." WE ALSO DISCLAIM ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. Some places don’t allow the disclaimers in this paragraph, so they may not apply to you.
+          <br /><br />
+
+          <strong>Limitation of Liability</strong>
+          <br /><br />
+
+          WE DON’T EXCLUDE OR LIMIT OUR LIABILITY TO YOU WHERE IT WOULD BE ILLEGAL TO DO SO—THIS INCLUDES ANY LIABILITY FOR LOK’S FRAUD OR FRAUDULENT MISREPRESENTATION IN PROVIDING THE SERVICE. IN COUNTRIES WHERE THE FOLLOWING TYPES OF EXCLUSIONS AREN’T ALLOWED, WE'RE RESPONSIBLE TO YOU ONLY FOR LOSSES AND DAMAGES THAT ARE A REASONABLY FORESEEABLE RESULT OF OUR FAILURE TO USE REASONABLE CARE AND SKILL OR OUR BREACH OF OUR CONTRACT WITH YOU. THIS PARAGRAPH DOESN’T AFFECT CONSUMER RIGHTS THAT CAN'T BE WAIVED OR LIMITED BY ANY CONTRACT OR AGREEMENT.
+          <br /><br />
+          IN COUNTRIES WHERE EXCLUSIONS OR LIMITATIONS OF LIABILITY ARE ALLOWED, LOK WON’T BE LIABLE FOR:
+        </Card.Text>
+
+        <ListGroup variant='flush'>
+          <ListGroup.Item>i. ANY INDIRECT, SPECIAL, INCIDENTAL, PUNITIVE, EXEMPLARY, OR CONSEQUENTIAL DAMAGES, OR</ListGroup.Item>
+          <ListGroup.Item>ii. ANY LOSS OF USE, DATA, BUSINESS, OR PROFITS, REGARDLESS OF LEGAL THEORY.</ListGroup.Item>
+        </ListGroup>
+        <br />
+
+        <Card.Text>
+          THESE EXCLUSIONS OR LIMITATIONS WILL APPLY REGARDLESS OF WHETHER OR NOT LOK OR ANY OF ITS AFFILIATES HAS BEEN WARNED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+          IF YOU USE THE SERVICE FOR ANY COMMERCIAL, BUSINESS, OR RE-SALE PURPOSE, LOK, ITS AFFILIATES, SUPPLIERS OR DISTRIBUTORS WILL HAVE NO LIABILITY TO YOU FOR ANY LOSS OF PROFIT, LOSS OF BUSINESS, BUSINESS INTERRUPTION, OR LOSS OF BUSINESS OPPORTUNITY. LOK AND ITS AFFILIATES AREN’T RESPONSIBLE FOR THE CONDUCT, WHETHER ONLINE OR OFFLINE, OF ANY USER OF THE SERVICES.
+
+          OTHER THAN FOR THE TYPES OF LIABILITY WE CANNOT LIMIT BY LAW (AS DESCRIBED IN THIS SECTION), WE LIMIT OUR LIABILITY TO YOU TO THE GREATER OF $0 USD.
+          <br /><br />
+
+          <strong>Resolving Disputes</strong>
+          <br /><br />
+
+          As Lok is a free service that you must opt into, with no forms of advertisement or pay-to-use features, we do not entertain anything in the form of legal case filings in lieu of the Service provided. 
+          <br /><br />
+
+          <strong>IF YOU'RE A U.S. RESIDENT, YOU ALSO AGREE TO THE FOLLOWING MANDATORY ARBITRATION/MEDIATION PROVISIONS:</strong>
+        </Card.Text>
+
+        <ListGroup variant='flush'>
+          <ListGroup.Item>- There will be no private arbitration or public mediation to handle any disputes related to Lok or its Service. Lok holds no responsibility for anything, and you agree to that by using the Service.</ListGroup.Item>
+          <ListGroup.Item>- <strong>NO CLASS ACTIONS</strong>. You may not not bring a claim as a plaintiff or a class member in a class, consolidated, or representative action. Class arbitrations, class actions, private attorney general actions, and consolidation with other arbitrations aren’t allowed.</ListGroup.Item>
+        </ListGroup>
+        <br />
+
+        <Card.Text>
+          <strong>Controlling Law</strong>
+          <br /><br />
+
+          These Terms will be governed by U.S. law except for its conflicts of laws principles. However, some countries (including those in the European Union) have laws that require agreements to be governed by the local laws of the consumer's country. This paragraph doesn’t override those laws.
+          <br /><br />
+
+          <strong>Entire Agreement</strong>
+          <br /><br />
+
+          These Terms constitute the entire agreement between you and Lok with respect to the subject matter of these Terms, and supersede and replace any other prior or contemporaneous agreements, or terms and conditions applicable to the subject matter of these Terms. These Terms create no third party beneficiary rights.
+          <br /><br />
+
+          <strong>Waiver, Severability & Assignment</strong>
+          <br /><br />
+
+          Lok’s failure to enforce a provision is not a waiver of its right to do so later. If a provision is found unenforceable, the remaining provisions of the Terms will remain in full effect and an enforceable term will be substituted reflecting our intent as closely as possible. You may not assign any of your rights under these Terms, and any such attempt will be void. Lok may assign its rights to any of its affiliates or subsidiaries, or to any successor in interest of any business associated with the Services.
+          <br /><br />
+
+          <strong>Modifications</strong>
+          <br /><br />
+
+          We may revise these Terms from time to time to better reflect:
+        </Card.Text>
+
+        <ListGroup variant='flush'>
+          <ListGroup.Item>(a) changes to the law,</ListGroup.Item>
+          <ListGroup.Item>(b) new regulatory requirements, or</ListGroup.Item>
+          <ListGroup.Item>(c) improvements or enhancements made to our Service.</ListGroup.Item>
+        </ListGroup>
+        <br />
+
+        <Card.Text>
+          If an update affects your use of the Service, we still reserve the right to withhold notification from you prior to the update's effective date.
+          <br /><br />
+          If you don’t agree to the updates we make, please cancel your account. By continuing to use or access the Service after the updates come into effect, you agree to be bound by the revised Terms.
+        </Card.Text>
+      </Card.Body>
+    </Card>
   )
 };
